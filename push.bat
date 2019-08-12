931fa03d7fd928792989877ee0884661044de7f4
Tom Bresee <Tom.Bresee@t-mobile.com>
2019-08-12 10:37:25 -0500

    -

---
+++ push.bat
@@ -0,0 +1,14 @@
+@ECHO OFF 
+:: This batch will upload files to github automatically
+TITLE GitPush
+:: Section 1: test batch file
+ECHO ============================
+ECHO Github Push
+ECHO ============================
+git add * 
+git commit -am "-"
+git push
+ECHO ============================
+ECHO done
+ECHO ============================
+PAUSE
